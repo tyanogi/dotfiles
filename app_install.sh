7f5b72622e5c720f598852b091422a8f9783ab13
tyanogi <b1444034@planet.kanazawa-it.ac.jp>
2020-05-26 11:02:04 +0900

    fix zsh path

--- app_install.sh
+++ app_install.sh
@@ -9,7 +9,7 @@ if [ $OS = 'ubuntu' ]; then
   sudo apt update
   sudo apt install -y vim-gtk tmux zsh git
   sed -i 's/.*default-shell/#&/g' $DOTPATH/.tmux.conf
-  chsh -s which zsh
+  chsh -s $(which zsh)
 elif [ $OS = 'mac' ]; then
   echo $OS
 else
